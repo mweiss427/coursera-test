d8ee3ca611d26f153c710c06eee8a87742b2765c
Matt Weiss <mweiss427@gmail.com>
2022-05-10 15:58:11 -0500

    take 5

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -114,6 +114,7 @@ function buildAndShowHomeHTML (categories) {
       // $dc.loadMenuItems('L')
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet;
+      console.log("version 5")
       var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", "'test'");
       console.log(homeHtmlToInsertIntoMainPage)
       // TODO: STEP 4: Insert the produced HTML in STEP 3 into the main page
